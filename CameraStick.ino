46699203e21f7824882929ba749afaf540ab8354
AlBundy33 <6294586+AlBundy33@users.noreply.github.com>
2021-04-11 16:44:56 +0200

    support for leonardo

--- CameraStick.ino
+++ CameraStick.ino
@@ -1,24 +1,38 @@
-#include <DigiMouse.h>
+#ifndef ARDUINO_AVR_LEONARDO
+#define DIGISPARK
+#endif
 
-#define BUTTON PB0
+#ifdef DIGISPARK
+#include <DigiMouse.h>
+#else
+#include <Mouse.h>
+#endif
+
+// define the PINS
+#define BUTTON PIN0
 #define AXIS_X A1
 #define AXIS_Y A0
-// to use A0 you have to enable the pin as input pin
-// https://wolles-elektronikkiste.de/digispark-die-bequeme-attiny85-alternative
-// https://digistump.com/wiki/digispark/tutorials/programming
-// /Applications/Arduino.app/Contents/Java/hardware/tools/avr/bin/avrdude -C /Applications/Arduino.app/Contents/Java/hardware/tools/avr/etc/avrdude.conf -v -p attiny85 -c avrisp -P /dev/cu.usbmodem14201 -b 19200 -n   
-// /Applications/Arduino.app/Contents/Java/hardware/tools/avr/bin/avrdude -C /Applications/Arduino.app/Contents/Java/hardware/tools/avr/etc/avrdude.conf -v -p attiny85 -c avrisp -P /dev/cu.usbmodem14201 -b 19200 -U hfuse:w:0x5F:m
 
+// define input behavior
 #define DEADZONE 20
-#define DELAY 10
+#define MOVE_DELAY 10
+#define SCROLL_DELAY 250
 #define ABS_RANGE 15
 
-int buttonPressTime = -1;
+// store states
+unsigned long buttonPressTime = 0L;
+unsigned long lastScrollTime = 0L;
+unsigned long lastMoveTime = 0L;
 bool scrollMode = false;
 
 void setup() {
   pinMode(BUTTON, INPUT_PULLUP);
+#ifdef DIGISPARK
   DigiMouse.begin();
+#else
+  Mouse.begin();
+  Serial.begin(9600);
+#endif
 }
 
 void loop() {
@@ -37,34 +51,69 @@ void loop() {
   x = map(x, -512, 512, -1 * ABS_RANGE, ABS_RANGE);
   y = map(y, -512, 512, -1 * ABS_RANGE, ABS_RANGE);
 
+  #ifndef DIGISPARK
+    Serial.print("x=");
+    Serial.print(x);
+    Serial.print(", y=");
+    Serial.print(y);
+    Serial.print(", button=");
+    Serial.print(buttonState ? "HIGH" : "LOW");
+    Serial.print(", buttonPressTime=");
+    Serial.print(buttonPressTime);
+    Serial.print(", scrollMode=");
+    Serial.print(scrollMode ? "true" : "false");
+    Serial.println();
+  #endif
+
   // check button
   if (buttonState == LOW) {
     // Button pressed
-    if (buttonPressTime < 0) {
+    if (buttonPressTime == 0L) {
       buttonPressTime = millis();
     }
   } else {
-    if (buttonPressTime > 0) {
+    if (buttonPressTime > 0L) {
       // Button released
-      if ((millis() - buttonPressTime) < 500) {
+      if (millis() - buttonPressTime < (long)500) {
         // short press - change camera
-        DigiMouse.setButtons(MOUSEBTN_LEFT_MASK);
-        DigiMouse.delay(20);
-        DigiMouse.setButtons(0);
+        #ifdef DIGISPARK
+          DigiMouse.setButtons(MOUSEBTN_LEFT_MASK);
+          DigiMouse.delay(20);
+          DigiMouse.setButtons(0);
+        #else
+          Mouse.click(MOUSE_LEFT);
+        #endif
       } else {
         // long press - switch mode
         scrollMode = !scrollMode;
       }
-      buttonPressTime = -1;
+      buttonPressTime = 0L;
     }
   }
 
   // move or scroll mouse
   if (x != 0 || y != 0) {
-    if (scrollMode)
-      DigiMouse.move(0, 0, y);
-    else
-      DigiMouse.move(x, y, 0);
-    DigiMouse.delay(DELAY);
+    unsigned long ms = millis();
+    if (scrollMode) {
+      if (ms - lastScrollTime > (long)SCROLL_DELAY) {
+        lastScrollTime = ms;
+        #ifdef DIGISPARK
+          DigiMouse.move(0, 0, y);
+          DigiMouse.update();
+        #else
+          Mouse.move(0, 0, y);
+        #endif
+      }
+    } else {
+      if (ms - lastMoveTime > (long)MOVE_DELAY) {
+        lastMoveTime = ms;
+        #ifdef DIGISPARK
+          DigiMouse.move(x, y, 0);
+          DigiMouse.update();
+        #else
+          Mouse.move(x, y, 0);
+        #endif
+      }
+    }
   }
 }
